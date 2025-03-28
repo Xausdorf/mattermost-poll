1b42d3f5943d0ee5192040779ca63652bb7b6121
Vasilii Parnikov <vasya.yakut.sakha@gmail.com>
2025-03-28 15:18:30 +0300

    add usecase

---
+++ internal/domain/poll.go
@@ -0,0 +1,35 @@
+package domain
+
+// Poll - structure for storing information about poll
+type Poll struct {
+	ID       int64
+	Question string
+	Options  []PollOption
+	IsActive bool
+	// Author - ID of poll's author
+	Author string
+}
+
+// PollOption - structure for storing poll's option and voters count
+type PollOption struct {
+	Text string
+	// Votes - count of users, who voted for this option
+	Votes int
+}
+
+// Answer - structure for connecting the user and his vote in the poll
+type Answer struct {
+	UserID string
+	PollID int64
+	// Vote - option number in the list of poll's options
+	Vote int
+}
+
+func NewPoll(question string, options []PollOption, author string) *Poll {
+	return &Poll{
+		Question: question,
+		Options:  options,
+		IsActive: true,
+		Author:   author,
+	}
+}
